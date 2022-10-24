57213305f46c977dff51308757632d2ca1bf08d1
CuongNguyen291201 <cuongnguyen291201@gmail.com>
2022-10-24 07:41:15 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import ChatBot from "react-simple-chatbot";
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  background: "#f5f8fb",
+  fontFamily: "Arial",
+  headerBgColor: "#1976d2",
+  headerFontColor: "#fff",
+  headerFontSize: "15px",
+  botBubbleColor: "#fff",
+  botFontColor: "black",
+  userBubbleColor: "#1976d2",
+  userFontColor: "#fff"
+};
+
+const steps = [
+  {
+    id: 1,
+    message: 'What number I am thinking?',
+    trigger: 2,
+  },
+  {
+    id: 2,
+    options: [
+      { value: 1, label: 'Number 1', trigger: 4 },
+      { value: 2, label: 'Number 2', trigger: 3 },
+      { value: 3, label: 'Number 3', trigger: 3 },
+    ]
+  },
+  {
+    id: 3,
+    message: 'Wrong answer, try again.',
+    trigger: 2,
+  },
+  {
+    id: 4,
+    message: 'Awesome! You are a telepath!',
+    end: true,
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <ChatBot
+        headerTitle="Hệ thống tư vấn đồ điện tử"
+        steps={steps}
+      />
+
+    </ThemeProvider>
   );
 }
 
